0e648190a2a94140abcec7e4a14c1575c4eccc27
kwameessien <180320488+kwameessien@users.noreply.github.com>
2026-03-02 19:18:58 -0500

    Moved MainPoster closer by adding a small negative top margin (mt-[-12px])

--- src/components/homepage/MainPoster.tsx
+++ src/components/homepage/MainPoster.tsx
@@ -5,7 +5,7 @@ type MainPosterProps = {
 
 function MainPoster({ imageSrc = '/assets/Byredo-AlwaysOnMakeup.png', alt = 'Byredo' }: MainPosterProps) {
   return (
-    <div className="w-full">
+    <div className="mt-[-12px] w-full">
       <img src={imageSrc} alt={alt} className="h-auto w-full object-cover" />
     </div>
   )
